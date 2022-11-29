e125746c68856d19c09bc8fa7778587afe87fe7e
Paige Sun <paige.sun.dev@gmail.com>
2022-11-29 13:52:07 -0800

    init: Add example of using TS to add text to index.

--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
-console.log('hello world!')
+const appRoot = document.getElementById('app');
+if (appRoot) {
+    const newContent = document.createTextNode("Hi there and greetings!");
+    appRoot.appendChild(newContent);
+}
